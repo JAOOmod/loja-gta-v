4efb05fe884c3df292053ff0ffb5225439632326
JAOOStore <joao.cfo2008@outlook.com>
2025-02-04 18:26:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.getElementById("alertButton").addEventListener("click", function() {
-    alert("Você será redirecionado para a página de compra!");
-});
+document.getElementById('alertButton').onclick = function() {
+    alert("Você clicou no botão!");
+};
